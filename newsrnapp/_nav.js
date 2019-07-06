bfbb4ed24d79e7389d9ec7beae9dc629ab376af2
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-07-06 23:45:52 +0530

    Initial Commit of React Native

---
+++ newsrnapp/_nav.js
@@ -0,0 +1,20 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+      },
+    },
+    {
+      name: 'Add News',
+      url: '/news/add',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+      },
+    },
+  ],
+};
---
+++ newsrnapp/app/modules/home/constants.js
@@ -0,0 +1,8 @@
+export const NAME = 'home';
+
+export const RETRIEVING_HEADLINES = 'home/RETRIEVING_HEADLINES';
+export const HEADLINES_AVAILABLE = 'home/HEADLINES_AVAILABLE';
+export const HEADLINES_ERROR = 'home/HEADLINES_ERROR';
+
+export const API_URL = 'https://newsapi.org/v2/top-headlines?';
+export const API_KEY = '24621fbb5fb443bc8e9191f7c568eb1a';
